a4b4ee0e623b3f7ac408e950afee5bef6c60b187
KyleHu14 <lloydpearce4@gmail.com>
2022-09-06 13:57:31 +0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div>Home</div>
+            <div>Resume</div>
+            <div>Projects</div>
+        </div>
+    );
+};
+
+export default Navbar;
